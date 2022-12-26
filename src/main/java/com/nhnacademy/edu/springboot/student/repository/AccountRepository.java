e3e7c0112f15d7c7ee4f10445e3f5b0a5fdd02e1
정세현 <vidaloca827@naver.com>
2022-12-26 14:18:37 +0900

    Spring Boot

--- src/main/java/com/nhnacademy/edu/springboot/student/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.edu.springboot.student.repository;
-
-import com.nhnacademy.edu.springboot.student.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
--- src/main/java/com/nhnacademy/edu/springboot/student/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.edu.springboot.student.repository;
-
-import com.nhnacademy.edu.springboot.student.model.Student;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
